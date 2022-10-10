8251937eac5427c53c0d0f9c7a18f64963099d7e
hoanam2k2 <hoanam2k2.tb@gmail.com>
2022-10-10 21:23:07 +0700

    gen routing module shop vs admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule) }, { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
